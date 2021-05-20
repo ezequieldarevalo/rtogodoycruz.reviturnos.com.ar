6ac4fa3264358f2e35d4931fdb79641e07974c18
Ezequiel Arevalo <ezequiel.arevalo@fravega.com.ar>
2021-05-20 13:49:54 -0300

    log agregado - 9

--- app/Models/Lune.php
+++ app/Models/Lune.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Lunes extends Model
+class Lune extends Model
 {
     use HasFactory;
 
