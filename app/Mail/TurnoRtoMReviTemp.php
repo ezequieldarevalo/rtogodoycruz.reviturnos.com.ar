0b3572cdc80cbc10383a5ffd6722b3bbdfb04dbe
ezequielarevalo <ezequiel.arevalo@kognitiv.com>
2022-11-06 16:23:44 +0000

    temp mail added

---
+++ app/Mail/TurnoRtoMReviTemp.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\TurnoRto;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TurnoRtoMReviTemp extends Mailable
+{
+    use Queueable, SerializesModels;
+
+
+    /**
+     * The order instance.
+     *
+     * @var \App\Models\Order
+     */
+    public $turnomail;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(TurnoRto $turnomail)
+    {
+        $this->turnomail = $turnomail;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Turno RTO Mendoza')->view('emails.nuevoturnorevitemp');
+    }
+}
