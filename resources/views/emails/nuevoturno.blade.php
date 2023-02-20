1d0c0ca5ce9126edc9a01256efeb771b2c14c00b
ezequielarevalo <ezequiel.arevalo@kognitiv.com>
2023-02-20 06:42:20 +0000

    san martin new mail format added

--- resources/views/emails/nuevoturno.blade.php
+++ resources/views/emails/nuevoturno.blade.php
@@ -11,17 +11,37 @@ html{
 }
 
 .titulo-general{
-    font-size: 18px;
-    font-weight: bold
+    font-size: 20px;
+    font-weight: bold;
+    text-decoration: underline;
 }
 
 .titulo-dato{
+    width: 80px;
     font-size: 15px;
-    text-decoration: underline
+    background-color: grey;
+    border-radius: 4px;
+    padding: 3px;
+    display: inline-block;
+    text-align:center;
+}
+
+.linea-dato{
+    margin-bottom: 10px;
 }
 
 .dato{
-    font-size: 12px
+    font-size: 14px
+}
+
+.dato-alert{
+    color: red;
+    font-weight: bold;
+    font-size: 16px
+}
+
+p{
+    line-height: 25px;
 }
 </style>
 </head>
@@ -30,18 +50,26 @@ html{
     <div class="cont">
         <span class="dato">Usted acaba de reservar un turno para realizar la RTO en la planta {{ $turnomail->plant_name }}</span>
         <br/><br/>
-        <div class="titulo-general">Detalles de su turno</div>
+        <div class="titulo-general">DETALLES DE SU TURNO</div>
+        <br/>
         
-        <span class="titulo-dato">Nombre:</span><span class="dato"> &nbsp;{{ $turnomail->nombre }}</span><br/>
-        <span class="titulo-dato">Dominio:</span><span class="dato"> &nbsp;{{ $turnomail->dominio }}</span><br/>
-        <span class="titulo-dato">Fecha:</span><span class="dato"> &nbsp;{{ $turnomail->fecha }}</span><br/>
-        <span class="titulo-dato">Hora:</span><span class="dato">&nbsp; {{ $turnomail->hora }}</span><br/>
-        <span class="titulo-dato">Id de turno:</span><span class="dato">&nbsp; {{ $turnomail->id }}</span><br/>
-
+        <div class="linea-dato">
+            <div class="titulo-dato">Nombre</div><span class="dato"> &nbsp;{{ $turnomail->nombre }}</span>
+        </div>
+        <div class="linea-dato">
+            <span class="titulo-dato">Dominio</span><span class="dato"> &nbsp;{{ $turnomail->dominio }}</span>
+        </div>
+        <div class="linea-dato">
+            <span class="titulo-dato">Fecha</span><span class="dato"> &nbsp;{{ $turnomail->fecha }}</span>
+        </div>
+        <div class="linea-dato">
+            <span class="titulo-dato">Hora</span><span class="dato"> &nbsp;{{ $turnomail->hora }}</span>
+        </div>
+        <div class="linea-dato">
+            <span class="titulo-dato">Id de turno</span><span class="dato"> &nbsp;{{ $turnomail->id }}</span>
+        </div>
         <!-- if payments is active -->
-        @if (!$turnomail->no_payment)
-	        <span class="dato">Recuerde que a partir de este momento cuenta con {{ $turnomail->time_to_pay }}hs para realizar el pago, de lo contrario, será automáticamente cancelado.</span><br/><span class="dato">Para realizar el pago si no lo hizo, puede hacerlo haciendo <a href="{{ $turnomail->url }}">click aquí</a>.</span>
-        @endif
+
         <p>
             <span class="dato">Si lo desea, puede cancelar su turno haciendo <a href="{{ $turnomail->cancel_quote_url }}">click aquí</a></span>
         </p>
